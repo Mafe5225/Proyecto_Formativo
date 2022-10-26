b2b6784a9619cdb73694f6c07e750a74e57f5002
DanielaGuzmanXD <ddguzman40@misena.edu.co>
2022-10-26 09:15:37 -0500

    main

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,19 @@ use App\Http\Controllers\ClientesController;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function () 
+{
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/dashboard', function () 
+{
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 Route::resource('clientes', ClientesController::class)->middleware('auth');
+// Route::resource('clientes', ClientesController::class)->middleware('auth');
 
 
 
